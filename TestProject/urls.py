6ba89181e878017553d9b4637ea6da76998b94c5
sisi <dqgsh1055@gmail.com>
2019-08-26 02:54:38 +0900

    시현_아이템기반 추천,분류 알고리즘 추천 코드 구현

--- TestProject/urls.py
+++ TestProject/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import hello, index, saveToDb, testsisi
+from .views import hello, index, saveToDb, testsisi,lambda_handler, itemCF
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home_recom/<humidity>/<temp>/<speed>/<emo1>/<emo2>', lambda_handler), #humidity,temp,speed,emotion
+    
     path('hello/<name>', hello),
     path('savetodb/<name>', saveToDb),
-    path('', index),
-    path('testsisi/', testsisi)
+    path('item_cf/<user_uuid>', itemCF), #userid말고 uuid를 주겠지 함수에서 변경해야 한다
+    path('testsisi/', testsisi),
+    path('', index) # 이게 기본url? 어쨌든 이 아래에 path()있으면 nginx에서 인식안된다 시발
+    
 ]
--- TestProject/views.py
+++ TestProject/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Name
+from .order_decisiontree_recom import order_decisionTree_recom
+from .item_collaborative_filtering import item_collaborativeFiltering
+
+
+def itemCF(request, user_uuid):#사용자1, 아이템1
+    
+    result = item_collaborativeFiltering(user_uuid) #str:dict
+    # dict으로 두 가지 종류 추천을 반환할거야
+    # 아이템:안먹은 유사아이템
+    # 사용자 > 안먹은 아이템  각각 하나씩
+    #result = "{'user_cf':"+item_cf_result+",'items_sim':"+items_sim_dict+"}"
+    return HttpResponse('<p>'+ result +'</p>') ## 마지막 str
+
+
+def lambda_handler(request,humidity,temp,speed,emo1,emo2):#사용자1
+    result_predict = order_decisionTree_recom(humidity,temp,speed,emo1,emo2)
+
+    return HttpResponse('<p>'+ result_predict +'</p>')
 
 
 def hello(request, name):
@@ -21,3 +39,4 @@ def index(request):
 
 def testsisi(request):
     return HttpResponse('<h1>장고 배포 성공</h1>')
+
